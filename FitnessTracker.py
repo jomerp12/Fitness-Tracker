041d60db28059effe4b44b9d312cfa257a271d83
Seungjoon Lee <sd529576@gmail.com>
2020-11-10 11:52:22 -0500

    more docstrings for the project

--- FitnessTracker.py
+++ FitnessTracker.py
@@ -21,7 +21,7 @@ class GetWorkout:
         preference and creates a tailored workout plan.
         
         Args:
-            workout(str):
+            workout(str): a string that will contain workout plans for the user.
         """
         
     def dietplan(self, Diet):
@@ -29,5 +29,25 @@ class GetWorkout:
         preference and creates a tailored diet plan.
         
         Args:
-            Diet(str):
+            Diet(str): a string that will contain diet plan for the user.
         """
+        
+    def video_tutorial(self,workout,Diet):
+        """ provides video_tutorial based on the specific exercise of workout/diet plan given by the methods.
+        Args:
+            workout(str): a string that will contain workout plans for the user.
+            Diet(str): a string that will contain diet plan for the user.
+        Returns:
+            exercise(dictionary): Defauilt value will be an empty dictionary and will add workout/diet plan as a key and url(web address) as a value.
+        """
+        
+def parse_args(arglist):
+    """ parse command-line arguments."""
+        
+def main(filename,workout,Diet):
+    """ provide a space to store our arguments and execute desired plan.
+    Args:
+        filename(str): contains the path to a .txt file containing a list of pre-made workout plans
+        workout(str): a string that will contain workout plans for the user.
+        Diet(str): a string that will contain diet plan for the user. 
+    """

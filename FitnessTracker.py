1e8840fb3a1d3bfd575a59aa42315dead50cb3f3
jomerp12 <jpaulino@terpmail.umd.edu>
2020-11-09 20:32:40 -0500

    Create FitnessTracker.py

---
+++ FitnessTracker.py
@@ -0,0 +1,33 @@
+class GetWorkout:
+    """Provides personalized workout schedules and diet plans.
+    
+    Attributes:
+    
+    
+    """
+    
+    def __init__(self, filename):
+        """Initializes a GetWorkout attribute and reads contents of the 
+        specified data file and populate workout plans.  
+        
+        Args:
+            filename(str): contains the path to a .txt file containing 
+            a list of pre-made workout plans
+
+        """
+        
+    def workoutplan(self, workout):
+        """Sorts keys of the GetWorkout attribute according to user's 
+        preference and creates a tailored workout plan.
+        
+        Args:
+            workout(str):
+        """
+        
+    def dietplan(self, Diet):
+        """Sorts keys of the GetWorkout attribute according to user's 
+        preference and creates a tailored diet plan.
+        
+        Args:
+            Diet(str):
+        """
